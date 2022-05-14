673d37d45b2c112b387a1b58c0289d9698d17e91
Ivan Kavalerov <ivan@kavalerov.net>
2022-05-14 14:00:33 +0100

    Host webform via Netlify to call the lambda, update lambda configuration to expose it behind API Gateway

--- main.tf
+++ main.tf
@@ -120,3 +120,63 @@ resource "aws_iam_user_policy" "retool_lambda_invoker_policy" {
   }
 EOF
 }
+
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "serverless_lambda_gw"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "serverless_lambda_stage"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "tf-generator" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.tf-generator.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "tf-generator" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /generate"
+  target    = "integrations/${aws_apigatewayv2_integration.tf-generator.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.tf-generator.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
--- tf-generator/lambda.py
+++ tf-generator/lambda.py
@@ -4,5 +4,6 @@ def lambda_handler(event, context):
 	# TODO implement
 	return {
 		'statusCode': 200,
-		'body': "https://github.com/alfonsof/terraform-aws-examples"
+		# 'body': "https://github.com/alfonsof/terraform-aws-examples"
+		'body': json.dumps(event['queryStringParameters'])
 	}
