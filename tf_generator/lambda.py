ec360a144ab734bc9822a82996f823bcfdc2c84e
Ivan Kavalerov <ivan@kavalerov.net>
2022-05-22 16:50:11 +0100

    Added sample lambda implementation

--- tf_generator/lambda.py
+++ tf_generator/lambda.py
@@ -1,10 +1,44 @@
+import argparse
 import json
+import os
+import json
+
+from dotenv import load_dotenv
+
+from tf_generator.generator import TerraformGenerator
+from tf_generator.models.s3_templates import ServiceProvider
+
+load_dotenv()
+
+TEMPLATES_MAP_PATH = os.path.join(os.getcwd(), "templates_map.json")
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("provider", choices=[a for a in ServiceProvider])
+    parser.add_argument("--compute", type=str)
+    parser.add_argument("--serverless", type=str)
+    parser.add_argument("--storage", type=str)
+    parser.add_argument("--database", type=str)
+    parser.add_argument("--website", type=str)
+
+    return parser
 
 
 def lambda_handler(event, context):
-    # TODO implement
+    args = event["queryStringParameters"]
+    with open(TEMPLATES_MAP_PATH, "r") as f:
+        template_map = json.load(f)
+    generator = TerraformGenerator(template_map)
+    templates = generator.get_templates(
+        provider=args.provider,
+        compute_service=args.compute,
+        serverless_service=args.serverless,
+        storage_service=args.storage,
+        database_service=args.database,
+        website_host_service=args.website,
+    )
     return {
         "statusCode": 200,
-        # 'body': "https://github.com/alfonsof/terraform-aws-examples"
-        "body": json.dumps(event["queryStringParameters"]),
+        "body": json.dumps(templates),
     }
