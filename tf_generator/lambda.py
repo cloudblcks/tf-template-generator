a22ff1a3eb8710ca0f2f134be99da13e87b5155d
Ivan Kavalerov <ivan@kavalerov.net>
2022-06-10 10:28:02 +0100

    Add branch in the lambda for more complex POST request

--- tf_generator/lambda.py
+++ tf_generator/lambda.py
@@ -13,24 +13,27 @@ TEMPLATES_MAP_PATH = os.path.join(os.getcwd(), "templates_map.json")
 
 def lambda_handler(event, context):
     args = event["queryStringParameters"]
-    print(args)
-    if args.get("hosting") == "lambda":
-        args["serverless"] = "lambda"
-    if args.get("static") == "yes":
-        args["static"] = "static"
+    if args is not None:
+        if args.get("hosting") == "lambda":
+            args["serverless"] = "lambda"
+        if args.get("static") == "yes":
+            args["static"] = "static"
 
-    with open(TEMPLATES_MAP_PATH, "r") as f:
-        template_map = json.load(f)
-    generator = TerraformGenerator(template_map)
-    templates = generator.generate_template(
-        provider=args.get("provider"),
-        compute_service=args.get("compute"),
-        serverless_service=args.get("serverless"),
-        storage_service=args.get("storage"),
-        database_service=args.get("db"),
-        website_host_service=args.get("static"),
-    )
-    return {
-        "statusCode": 200,
-        "body": templates,
-    }
+        with open(TEMPLATES_MAP_PATH, "r") as f:
+            template_map = json.load(f)
+        generator = TerraformGenerator(template_map)
+        templates = generator.generate_template(
+            provider=args.get("provider"),
+            compute_service=args.get("compute"),
+            serverless_service=args.get("serverless"),
+            storage_service=args.get("storage"),
+            database_service=args.get("db"),
+            website_host_service=args.get("static"),
+        )
+        return {
+            "statusCode": 200,
+            "body": templates,
+        }
+    else:
+        data = event["body"]["cloudblocks_data"]
+        return {"statusCode": 200, "body": json.dumps(event)}
