31b87e4ac95b09acaa991ee8e12267052fc63ae0
Ivan Kavalerov <ivan@kavalerov.net>
2022-08-05 15:00:32 +0100

    Fix testing script

--- tests/build_sample_terraforms.sh
+++ tests/build_sample_terraforms.sh
@@ -5,5 +5,5 @@ cd tf_generator
 for file in tests/samples/*
 do
   echo Building $file
-  python cli.py build -f $file -o ../temp/$(basename $file).tf
+  python cli.py build -f $file -o /temp/$(basename $file).tf
 done
