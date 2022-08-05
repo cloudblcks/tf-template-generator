dd04f6d5476734d91c0e6b3698add0754dc2a524
Ivan Kavalerov <ivan@kavalerov.net>
2022-08-05 15:05:07 +0100

    Additional fixes

--- tests/build_sample_terraforms.sh
+++ tests/build_sample_terraforms.sh
@@ -4,5 +4,5 @@ mkdir temp/
 for file in tests/samples/*
 do
   echo Building $file
-  python cli.py build -f $file -o /temp/$(basename $file).tf
+  python cli.py build -f $file -o temp/$(basename $file).tf
 done
